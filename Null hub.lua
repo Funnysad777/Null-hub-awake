4c7967626077ac727fe4739233456dbe7365b9fd
Funnysad777 <funnysad777@gmail.com>
2025-01-24 17:00:25 +0700

    Update Null hub.lua

--- Null hub.lua
+++ Null hub.lua
@@ -306,10 +306,11 @@ end
 -- ขั้นตอนการอัปเกรด
 local upgrades = {
     {name = "Upgraded Titan Cameraman", gold = 1500, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = nil},
-    {name = "Upgraded Titan Cameraman2", gold = 3000, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman"},
-    {name = "Upgraded Titan Cameraman3", gold = 4000, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman2"},
-    {name = "Upgraded Titan Cameraman4", gold = 8000, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman3"},
-    {name = "Upgraded Titan Cameraman5", gold = 10000, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman4"}
+    {name = "Upgraded Titan Cameraman2", gold = 1500, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman"},
+    {name = "Upgraded Titan Cameraman3", gold = 3000, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman2"},
+    {name = "Upgraded Titan Cameraman4", gold = 4000, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman3"},
+    {name = "Upgraded Titan Cameraman5", gold = 8000, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman4"},
+    {name = "Upgraded Titan Cameraman6", gold = 10000, position = CFrame.new(-89.8586349, -34.0955658, -29.0876522), angles = CFrame.Angles(0, 0, 0), tower = "Upgraded Titan Cameraman5"}
 }
 
 -- ทำงานตามขั้นตอน
